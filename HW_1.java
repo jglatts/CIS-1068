77bb4909d6812cb1d2782ae1e1cbf64325b30e1d
John Glatts <john@z-axiscc.com>
2019-09-02 13:52:23 -0400

    Update HW_1.java

--- HW_1.java
+++ HW_1.java
@@ -1,115 +1,198 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package HelloWorld;
+html {
+  /* Load this*/
+  background: url(trianglify-lowres.png) repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  font-family: 'Roboto', sans-serif;
 
-/**
- *
- * @author John Glatts
- * @due-date 
- */
-public class Main {
+}
 
-    /* Format and print the letter H to console */    
-    public static void printH() {
-        System.out.println("\nH\tH");
-        System.out.println("H\tH");
-        System.out.println(" HHHHHHH");
-        System.out.println("H\tH");
-        System.out.println("H\tH");
-    }
-    
-    /* Format and print the letter E to console */    
-    public static void printE() {
-        System.out.println("\nEEEEEEEEE");
-        System.out.println("E");
-        System.out.println("EEEEE");
-        System.out.println("E");
-        System.out.println("EEEEEEEEE");
+/* Responsive header + image */
+.header {
+  overflow: hidden;
+  background: rgb(220, 220, 220); /* Fallback for older browsers without RGBA-support */
+  background: rgba(220, 220, 220, 0.7);
+  border: 6px solid whitesmoke;
+  padding: 20px 10px;
+  padding-left: 30px;
+  width: 25em;
+  margin: auto;
+  /*
+  margin-left: 10em;
+  margin-right: 60em;
+  */
+}
 
-    }
-    
-    /* Format and print the letter L to console */    
-    public static void printL() {
-        System.out.println("\nL");
-        System.out.println("L");
-        System.out.println("L");
-        System.out.println("L");
-        System.out.println("LLLLLLLLL");
-    }
-    
-    /* Format and print the letter O to console */
-    public static void printO() {
-        System.out.println("\nOOOOOOOOO");
-        System.out.println("O\tO");
-        System.out.println("O\tO");
-        System.out.println("O\tO");
-        System.out.println("O\tO");
-        System.out.println("OOOOOOOOO");
-    }
-        
-    /* Format and print a comma to console */
-    public static void printComma() {
-        System.out.println("\n\n\n\n,,");
-        System.out.println(",");
-    }
+.header a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 25px;
+  border-radius: 4px;
+}
 
-    /* Format and print the letter W to console */
-    public static void printW() {
-        System.out.println("W\tWW\tW");
-        System.out.println("W\tWW\tW");
-        System.out.println("   WW       WW  ");
-        System.out.println("   WW       WW  ");
-        System.out.println("    W     W   ");
-    }
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.header-right {
+  float: right;
+}
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+
+  .column {
+    float: center;
+    height: 450px;
+    margin-left: 2em;
+    margin-right: 4px;
+    margin-top: 1em;
+    width: 70%;
+    padding: 30px;
+    border-radius: 25px;
+    border: 4px solid whitesmoke;
+    background: rgb(220, 220, 220); /* Fallback for older browsers without RGBA-support */
+    background: rgba(220, 220, 220, 0.7);
+    /* Get rid of style tag in html and change color here*/
+}
+  
+  .header-right {
+    float: none;
+  }
+  .header {
+      margin-right: 1em;
+  }
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+h2 {
+}
+
+
+
+h2:hover {
+    color: darkmagenta;
+}
+
+#selfie {
+    height: 8em;
+    max-width: 100%;
+    margin-left: 15em;
+    margin-top: -18em;
+    /*border-bottom-right-radius: 15px;*/
+    border-radius: 25px;
+    /*border-top-right-radius: 25px;*/
+}
+
+#move-bio-top {
+  background: rgb(220, 220, 220); /* Fallback for older browsers without RGBA-support */
+  background: rgba(220, 220, 220, 0.7);
+  height: 10em;
+  margin-left: 25em;
+  margin-right: 32em;
+}
+
+
+.name {
+    margin-top: -3em;
+    margin-left: 2em;
+    font-size: 3vw;
+    padding-left: 4px;
+    text-align: center;
+}
+
+.lil-bio {
+    /* Having some trouble getting alignment right */
+    margin-top: -1em;
+    margin-left: 4em;
+    text-align: center;
+    color: black;
+}
+
+#move_down_boxes {
+    margin-top: 5px;
+}
+
+.text-change {
+    font-size: 1vw;
+}
+
+* {
+  box-sizing: border-box;
+
+}
+
+/* Create three equal columns that floats next to each other */
+.column {
+  float: left;
+  height: 450px;
+  margin-left: 2em;
+  margin-right: 4px;
+  margin-top: 1em;
+  width: 30%;
+  padding: 30px;
+  border-radius: 25px;
+  border: 4px solid whitesmoke;
+  background: rgb(220, 220, 220); /* Fallback for older browsers without RGBA-support */
+  background: rgba(220, 220, 220, 0.7);
+  /* Get rid of style tag in html and change color here*/
+}
+
+/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 1000px) {
+
+  .text-change {
+    font-size: .7em
+  }
+  
+  .column {
+    width: 80%;
+    font-size: 1.5em
+  }
+  
+  /* Create three equal columns that floats next to each other */
+.column {
+  float: left;
+  height: 450px;
+  margin-left: 2em;
+  margin-right: 4px;
+  margin-top: 1em;
+  width: 30%;
+  padding: 30px;
+  border-radius: 25px;
+  border: 4px solid whitesmoke;
+  background: rgb(220, 220, 220); /* Fallback for older browsers without RGBA-support */
+  background: rgba(220, 220, 220, 0.7);
+  /* Get rid of style tag in html and change color here*/
+}  
     
-    /* Format and print the letter R to console */
-    public static void printR() {
-        System.out.println("\nRRRRRRR");
-        System.out.println("R\tR");
-        System.out.println("RRRRRRR");
-        System.out.println("R\tR");
-        System.out.println("R      R");
-    }
-    
-    /* Format and print the letter D to console */
-    public static void printD() {
-        System.out.println("\nDDDDDDDD");
-        System.out.println("D\tD");
-        System.out.println("D\tD");
-        System.out.println("D\tD");
-        System.out.println("D    D");
-    }
-    
-    /* Format and print exclamation point to console */
-    public static void printPoint() {
-        System.out.println("\n!!");
-        System.out.println("!!");
-        System.out.println("!!");
-        System.out.println(" ");    // blank line
-        System.out.println("!!");
-    }
-    
-    /* Call all helper fn()s and print to console */
-    public static void printHelloWorld() {
-        printH();
-        printE();
-        printL();
-        printL();
-        printO();
-        printComma();
-        printW();   
-        printO();
-        printR();
-        printL();
-        printD();
-        printPoint();    
-    }
-    
-    /* Program entry point */
-    public static void main(String[] args) {
-        printHelloWorld();
-    }
+  #box-3 {
+    margin-bottom: 4em;
+  }
+  
 }
