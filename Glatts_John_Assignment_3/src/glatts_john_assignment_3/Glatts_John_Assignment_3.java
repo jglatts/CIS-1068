14628e24dd78b53e9269ee542676a2f912c72da0
John Glatts <john@z-axiscc.com>
2019-09-27 18:32:23 -0400

    Update Glatts_John_Assignment_3.java

--- Glatts_John_Assignment_3/src/glatts_john_assignment_3/Glatts_John_Assignment_3.java
+++ Glatts_John_Assignment_3/src/glatts_john_assignment_3/Glatts_John_Assignment_3.java
@@ -1,43 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package glatts_john_assignment_3;
+    package glatts_john_assignment_3;
 
-import java.util.Scanner;
+    import java.util.Scanner;
 
-/**
- *
- * @author John Glatts
- * @duedate 9-27-19
- * 
- */
-public class Glatts_John_Assignment_3 {
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        /*
-        Scanner scanner = new Scanner(System.in);
-        Scanner pigScanner = new Scanner (System.in);
-        System.out.println("\nFraction sum of " + 15 + " = " + fractionSum(15));
-        //smallestLargest(scanner); // test this with some randomly gen. int's
-        System.out.println("\nBinary represention of: " + 44 + " is " + toBinary(44));
-        System.out.println("\nBinary represention of: " + 420 + " is " + toBinary(420));
-        showTwos(7);
-        showTwos(18);
-        showTwos(24);
-        showTwos(188);
-        System.out.println("\n\nPascals Triangle");
-        // tweak both of these
-        pascalsTriangle();
-        System.out.println("\n\n");
-        pigLatin(pigScanner);
-       */
-        Scanner pigScanner = new Scanner (System.in);
-        pigLatin(pigScanner);
+    public class Glatts_John_Assignment_3 {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            Scanner plScanner = new Scanner(System.in);
+
+            // Chapter 4 Exercise 1
+            startFractionSum(scanner);
+
+            // Chapter 4 Exercise 8
+            smallestLargest(scanner); 
+
+            // Chapter 4 Programming Project 7
+            pascalsTriangle();  
+
+            // Chapter 5 Exercise 1
+            showTwos(7);
+            showTwos(18);
+            showTwos(68);
+            showTwos(120);
+
+            // Chapter 5 Exercise 3
+            System.out.println("\nBinary represention of: " + 44 + " is " + toBinary(44));
+
+            // Chapter 5 Programming Project 1
+            pigLatin(plScanner);
+    }
+
+    /*
+        Prompt the user to enter a value to be used as a fraction sum
+    */     
+    public static void startFractionSum(Scanner scanner) {
+            System.out.print("\nThis is the fractionSum program. What does n = ? ");
+            int n = scanner.nextInt();
+            System.out.println("\nFraction sum of " + n + " = " + fractionSum(n));
     }
     
     /*  
@@ -52,13 +51,13 @@ public class Glatts_John_Assignment_3 {
         
         return sum;
     }
-    
+
      /*  
         Prompt the user how many integers they would like to enter
         Scan each integer in and then find the smallest and largest values
     */
     public static void smallestLargest(Scanner scanner) {
-        System.out.print("\nHow many numbers would you like to enter?  ");
+        System.out.print("\nHow many numbers would you like to enter? ");
         int numberLimit = scanner.nextInt();
         int[] inputArray = new int[numberLimit];
         
@@ -68,71 +67,86 @@ public class Glatts_John_Assignment_3 {
             System.out.println("");
         }
         
-        System.out.println("Smallest Number = " + findValue(inputArray, inputArray[0], "smallest"));
-        System.out.println("Largest Number = " +   findValue(inputArray, inputArray[0], "largest"));
+        System.out.println("Smallest Number = " + getSmallestValue(inputArray));
+        System.out.println("Largest Number = " +   getLargestValue(inputArray));
         
-    }
+    }    
     
      /*  
-        Find and return the largest or smallest number from the array of integers 
+        Find and return the smallest integer from the given array
     */     
-    public static int findValue(int arr[], int firstValue, String check) {
-        int value = 0;
+    public static int getSmallestValue(int arr[]) {
+        int value = arr[0]; 
         
         for (int i = 0; i < arr.length; ++i) {
-            if (check.equals("smallest")) {
-                if (arr[i] < firstValue) value = arr[i];
-            }
-            else {
-                if (arr[i] > firstValue) value = arr[i];
-            }
-        }
+                    if (arr[i] < value) value = arr[i];              
+        }    
+        
         return value;
-    }
-
-     /*  
-          Accept an integer as a paramter and return the binary value  
-    */         
-    public static String toBinary(int intValue) {
-        return (String)Integer.toBinaryString(intValue);
     }
     
      /*  
-           Format and print pascals triangle  
-                - the values seem to be right, but the formatting is way off
-                - use proper formatting, i.e 4 space width padding    
+        Find and return the largest integer from the given array
+    */     
+     public static int getLargestValue(int arr[]) {
+        int value = arr[0];
+        
+        for (int i = 0; i < arr.length; ++i) {
+                if (arr[i] > value) value = arr[i]; 
+        }    
+        
+        return value;
+    }
+    
+     /*  
+          Accept an integer parameter and return the binary value in a string 
+    */         
+    public static String toBinary(int intValue) {
+        String returnStr = "";
+        while(intValue > 0) {
+            returnStr = ((intValue % 2 == 0) ? "0" : "1") + returnStr;
+            intValue /= 2;
+        }
+        return returnStr;
+    }
+    
+     /*  
+           Prompt the user and ask how many rows of pascals triangle they would like to print
+           Once a value is scanned, format and print pascals triangle 
     */   
     public static void pascalsTriangle() {
-        // see if 3 scanner objects are really needed
-        Scanner pascalScanner = new Scanner(System.in);
-        int  count = 1;
+        Scanner pascalScanner = new Scanner(System.in); 
+        int  lineNumbers, count = 1;
         
         System.out.print("\nThis is the pascalsTriangle program. How many lines of the triangle would you like? ");
-        int noRow = pascalScanner.nextInt();
+        lineNumbers = pascalScanner.nextInt();
         
-        // the values are correct
-        // calculate how many spaces to print
-        // the 2nd loop might already be doing this 
-        for(int i = 0; i < noRow; i++) {
-            for(int x = 1; x <= noRow-i; x++) {
-                System.out.print(" ");
+        for(int i = 0; i < lineNumbers; i++) {
+            for(int x = 1; x < lineNumbers - i; x++) {
+                System.out.printf("  "); 
             }
             for(int j = 0; j <= i; ++j)  {
-                if (j==0 || i==0)
+                if (j == 0 || i == 0) {
                     count = 1;
-                else
-                   count = (count * (i - j + 1))  /  j;
-                System.out.print(" " + count);
+                }    
+                else {
+                    // compute new value 
+                    count = (count * (i - j + 1))  /  j;
+                }
+                // print new value
+                System.out.printf("%4d",  count);
             }
            System.out.print("\n");
-        }    
+        }  
+        // new line after triangle is printed
+        System.out.println("\n");
     }
     
      /*  
           Compute the factors of 2 for an integer parameter  
     */      
     public static void showTwos(int value) {
-        System.out.print("\nFactors of 2 for: " + value + " = ");
+        System.out.print("Factors of 2 for: " + value + " = ");
         while (value % 2 == 0) {
             System.out.print("2 * ");
             value /= 2;
@@ -145,45 +159,54 @@ public class Glatts_John_Assignment_3 {
           and print the converted string
     */          
     public static void pigLatin(Scanner scanner) {
-        System.out.print("\nEnter a string to be converted to pig latin: ");
-        System.out.println("s");
-        //String str = scanner.nextLine();
-        System.out.print("\nPig Latin = " + translateToPigLatin("the deepest shade of mushroom blue") + "\n");
+        String str = "";
+        // only perform conversion if string is not empty 
+        do {
+                    System.out.print("\nEnter a string to be converted to pig latin: ");
+                     str = scanner.nextLine();       
+                     System.out.println("String converted to pig latin = " + translateToPigLatin(str));
+        } while (!str.equals(""));
+        System.out.println("Empty string passed in.\nProgram Terminated.");
     }
     
-    
+    /* 
+        Translate the given string to to piglatin
+        Return the converted string 
+    */
     public static String translateToPigLatin(String str) {
-        String outPut = "";
+        String outPut = " ";
         String segment[] = str.split(" ");
-        
+        char segmentIndex;
+
         for (int i = 0; i < segment.length; ++i) {
-            for (int y = 0; y < segment[i].length(); ++y) {
-                char segmentIndex = segment[i].charAt(y);
-                if (checkForVowel(segmentIndex)) {
-                     if (y != 0) {
-                            String toMove = segment[i].substring(0, 2); 
-	      String cutHere = segment[i].substring(y, segment[i].length()); 
-                            outPut += swapLetters(cutHere, toMove);
-                            break;
-	  }
-	  else {
-	       outPut += segment[i] + "-ay ";
-	  }                    
+                for (int y = 0; y < segment[i].length(); ++y) {
+                     segmentIndex = segment[i].charAt(y);
+                     if (checkForVowel(segmentIndex)) 
+                         if (y != 0) {
+                                String toMove = segment[i].substring(0, y);     
+                                String cutHere = segment[i].substring(y, segment[i].length()); 
+                                // add an if here to check if the word begins with a conosat, like th     
+                                outPut += cutHere + "-"+ toMove + "ay ";
+                                break;   
+                        }
+                        else 
+                            outPut += segment[i] + "-ay ";
                 }
-             }
-          }
-        return outPut;   
+           }
+        return outPut += "\n";   
     }  
 
-    // method is working 
+    /*
+        Check if a given letter is a vowel 
+    */
     public static boolean checkForVowel(char charSegment) {
-        return (charSegment == 'a' || charSegment == 'e' || charSegment == 'i' || charSegment == 'o' || charSegment == 'u');
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        
+        for (int i = 0; i < vowels.length; ++i) {
+            if (charSegment == vowels[i]) 
+                return true;
+        }
+        return false;
     }
-    
-    // seems like this method is working 
-    public static String swapLetters(String cut, String move) {
-        String returnStr = cut + move + "-ay ";
-        return returnStr;
-    }    
 
 }
