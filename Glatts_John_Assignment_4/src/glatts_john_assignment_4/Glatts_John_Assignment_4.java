60e9a761d471b8c8d8b91dc5cc07e43d4915242e
John Glatts <john@z-axiscc.com>
2019-10-13 18:06:36 -0400

    Update Glatts_John_Assignment_4.java

--- Glatts_John_Assignment_4/src/glatts_john_assignment_4/Glatts_John_Assignment_4.java
+++ Glatts_John_Assignment_4/src/glatts_john_assignment_4/Glatts_John_Assignment_4.java
@@ -12,11 +12,22 @@ public class Glatts_John_Assignment_4 {
     public static final String tokenPath = "tokens.txt";
     public static final String storyPath = "story.txt";
     
-    /*
-    ToDo:
-        - myers-bridges jawn
-        - add comments
-    */
+     // Myers - Brigss globals
+     public static int valueZero = 0; 
+     public static int set21 = 1; // test the set 2 of 2'nd row of each
+     public static int set22 = 2; // test the set 2 of 3'rd row of each
+     public static int set31 = 3; // test the set 3 of 4'th row of each
+     public static int set32 = 4; // test the set 3 of 5'th row of each
+     public static int totalQuestSet = 7; // to test the last value of
+     static int set1_A = 0; // count extrovert
+     static int set1_B = 0; // count introvert
+     static int set2_A = 0; // count sense
+     static int set2_B = 0; // count intuition
+     static int set3_A = 0; // count thinking
+     static int set3_B = 0; // count feeling
+     static int set4_A = 0; // count judge
+     static int set4_B = 0; // count perception
+    
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Scanner scanner = new Scanner(new File(numbersPath));
         Scanner statsScanner = new Scanner(new File(tokenPath));
@@ -27,6 +38,7 @@ public class Glatts_John_Assignment_4 {
         System.out.println("Std-Dev is: " + stdDev(devArray) + "\n");   
         generateMatrices();
         startStory(storyScanner);
+        startMyersBriggs(); // kind of working only got one output doe
     }
 
     /* 
@@ -48,7 +60,7 @@ public class Glatts_John_Assignment_4 {
     public static void printNumberInfo(int[] allValues, int index) {
         int evens = countEvens(allValues, index);
         System.out.println("\nLooking at 'numbers.txt'");
-        System.out.printf("\t-File has "+index + " numbers, sum = " + getSum(allValues));
+        System.out.printf("\t-File has "+ index + " numbers, sum = " + getSum(allValues));
         System.out.print(", " + evens + " evens " );
         // convert to %
         System.out.printf( "%.2f" , getPercent(evens, index));
@@ -280,5 +292,177 @@ public class Glatts_John_Assignment_4 {
         newStr +=" " + scanner.next();           
         return newStr;
     }
+
+    /*
+        Start the Myers-Briggs program and prompt the user for a input file
+        Once a file is verified and exists, convert the scores and display
+    */
+    public static void startMyersBriggs() throws FileNotFoundException {
+          Scanner console = new Scanner(System.in);
+          File file;
+          do {
+              System.out.print("\nEnter file name to process: ");
+              String fileName = console.nextLine();
+              file = new File(fileName);
+          } while (!file.exists());    
+          System.out.println();
+          evaluateAnswerFile(file);
+     }    
+
+    /*
+        Get the scores from the input file for Myers-Briggs
+    */
+     public static void evaluateAnswerFile(File f) throws FileNotFoundException  {
+          Scanner input = new Scanner(f);
+          String nameOne, answersOne, nameTwo, answersTwo;
+          nameOne = input.nextLine();
+          System.out.println(nameOne);
+          answersOne = input.nextLine().toUpperCase();
+          nameTwo = input.nextLine();
+          answersTwo = input.nextLine().toUpperCase();
+          calculateMyersBriggsResults(answersOne);
+          displayResult();
+          // calculate the second user
+          resetScores();
+          calculateMyersBriggsResults(answersTwo);
+          System.out.println(nameTwo);
+          displayResult();
+     }
+     
+     /*
+        Update the global vars, the scores, and then display
+     */
+     public static void calculateMyersBriggsResults(String answer) {
+                  for (int i = 0; i < answer.length(); i++)  {
+                        if ((i == valueZero) || ((i % totalQuestSet == valueZero))) {
+                             if (answer.charAt(i) == 'A') {
+                                  set1_A++;
+                             }
+                             if (answer.charAt(i) == 'B') {
+                                  set1_B++;
+                             }
+                        }
+                        else if (checkFirstSet(i)) {
+                             if (answer.charAt(i) == 'A'){
+                                  set2_A++;
+                             }
+                             if (answer.charAt(i) == 'B') {
+                                  set2_B++;
+                             }
+                        }
+                        else if (checkSecondSet(i))  {
+                             if (answer.charAt(i) == 'A')  {
+                                  set3_A++;
+                             }
+                             if (answer.charAt(i) == 'B') {
+                                  set3_B++;
+                             }
+                        }
+                        else {
+                             if (answer.charAt(i) == 'A') {
+                                  set4_A++;
+                             }
+                             if (answer.charAt(i) == 'B') {
+                                  set4_B++;
+                             }
+                        }
+                    }     
+     }
+
+     /*
+        Display the results of the Myers-Briggs conversion
+     */
+     public static void displayResult() {
+          double set1Per, set2Per, set3Per, set4Per;
+          String status = "";
+          status += set1_A + "A-" + set1_B + "B ";
+          status += set2_A + "A-" + set2_B + "B ";
+          status += set3_A + "A-" + set3_B + "B ";
+          status += set4_A + "A-" + set4_B + "B ";
+          set1Per = (double) set1_B / (set1_A + set1_B) * 100;
+          set2Per = (double) set2_B / (set2_A + set2_B) * 100;
+          set3Per = (double) set3_B / (set3_A + set3_B) * 100;
+          set4Per = (double) set4_B / (set4_A + set4_B) * 100;
+          status += "[" + (int) set1Per + "%, " + (int) set2Per + "%, " + (int) set3Per + "%, " + (int) set4Per + "%] = ";
+          status += getStatus(set1Per, 1) + getStatus(set2Per, 2) + getStatus(set3Per, 3) + getStatus(set4Per, 4);
+          System.out.print(status);
+          System.out.println();
+     }
+     
+     /*
+        Check and return the status of the input scores 
+     */
+     public static String getStatus(double percent, int set) {
+          // if it is set1
+          if (set == 1) {
+              if (percent > 50)
+                   return "I";
+              else if (percent < 50)
+                   return "E";
+              else
+                   return "X";
+          }
+          if (set == 2) {
+              if (percent > 50)
+                   return "N";
+              else if (percent < 50)
+                   return "S";
+              else
+                   return "X";
+          }
+          if (set == 3) {
+              if (percent > 50)
+                   return "F";
+              else if (percent < 50)
+                   return "T";
+              else
+                   return "X";
+          }
+          if (set == 4) {
+              if (percent > 50)
+                   return "P";
+              else if (percent < 50)
+                   return "J";
+              else
+                   return "X";
+          }
+          return "";
+     }    
+     
+     /*
+        Reset the global vars to start another score conversion
+     */
+     public static void resetScores() {
+            valueZero = 0; 
+            set21 = 1; 
+            set22 = 2; 
+            set31 = 3; 
+            set32 = 4; 
+            totalQuestSet = 7; 
+            set1_A = 0; 
+            set1_B = 0; 
+            set2_A = 0; 
+            set2_B = 0; 
+            set3_A = 0; 
+            set3_B = 0; 
+            set4_A = 0; 
+            set4_B = 0; 
+     }
+     
+     /*
+        Check each input score with the first set
+     */
+     public static boolean checkFirstSet(int i) {
+        return  (i - set21) == valueZero || (i - set22) == valueZero || (i - set21) % totalQuestSet == valueZero 
+                    || (i - set22) % totalQuestSet == valueZero;        
+     }
+ 
+      /*
+        Check each input score with the second set
+     */
+     public static boolean checkSecondSet(int i ) {
+        return (i - set31) == valueZero || (i - set32) == valueZero || (i - set31) % totalQuestSet == valueZero 
+                    || (i - set32) % totalQuestSet == valueZero;
+     } 
      
 }
