88ef373b4d0eecfc8df2cdf904da1a1f9049f8b4
John Glatts <john@z-axiscc.com>
2019-10-30 20:25:15 -0400

    Update Glatts_John_Assignment_6.java

--- Glatts_John_Assignment_6/src/glatts_john_assignment_6/Glatts_John_Assignment_6.java
+++ Glatts_John_Assignment_6/src/glatts_john_assignment_6/Glatts_John_Assignment_6.java
@@ -1,14 +1,85 @@
 package glatts_john_assignment_6;
 
+import java.util.Random;
+
 public class Glatts_John_Assignment_6 {
 
+    /*
+        Main Driver Function
+        ToDo 
+            - add prompts at each ex. saying what problem it is 
+    */
     public static void main(String[] args) {
-        WalkUpTicket test = new WalkUpTicket(123);
-        AdvanceTicket n = new AdvanceTicket(124, 13);
-        StudentAdvanceTicket x = new StudentAdvanceTicket(125, 9);
-        System.out.println("\n" + test.toString());
-        System.out.println("\n" + n.toString());
-        System.out.println("\n" + x.toString());
+        // Ch. 9, 5-7
+        checkTicketPrice();
+        
+        // Ch. 9, 8
+        MinMaxAccount a = new MinMaxAccount(new Startup("test"));
+        System.out.println("\nThe minimum balance is " + a.getMin() + " pennies");
+        System.out.println("The maximum balance is " + a.getMax() + " pennies");
+
+        // Ch. 9, 10
+        checkGroceryBill();
+    }
+    
+    public static void checkTicketPrice() {
+        Random r = new Random();
+        int count = 1;  // change this to a dynamic number 
+        double price = Ticket.getPrice();
+        switch ((int)price) {
+            case 50:
+                    WalkUpTicket walkUpTicket = new WalkUpTicket(count);
+                    System.out.println(walkUpTicket.toString());
+                    break;
+            case 40:
+                {
+                    AdvanceTicket advanceTicket = new AdvanceTicket(count, 7);
+                    System.out.println(advanceTicket.toString());
+                    break;
+                }
+            case 30:
+                {
+                    AdvanceTicket advanceTicket = new AdvanceTicket(count, 12);
+                    System.out.println(advanceTicket.toString());
+                    break;
+                }
+            case 20:
+                {
+                    StudentAdvanceTicket studentAdvanceTicket = new StudentAdvanceTicket(count, 7);
+                    System.out.println(studentAdvanceTicket.toString());
+                    break;
+                }
+            case 15:
+                {
+                    StudentAdvanceTicket studentAdvanceTicket = new StudentAdvanceTicket(count, 12);
+                    System.out.println(studentAdvanceTicket.toString());
+                    break;
+                }
+            default:
+                {
+                    System.out.println("Invalid Ticket Price");
+                    break;
+                }
+        }
+    }
+    
+    public static void checkGroceryBill() {
+        GroceryBill g = new GroceryBill(new Employee("Bob"));
+        DiscountBill gPoly = new DiscountBill(new Employee("Joe"), true);
+        
+        // no discount bill
+        g.addItem(new Item("apple", .75));
+        g.addItem(new Item("orange", .50));
+        g.addItem(new Item("steak", 50.00));
+        g.addItem(new Item("beer", 15.00));
+        g.printReceipt();
+        System.out.println("Total Price = $" + g.getTotal() + " - no discount added");
+        
+        // bill with discounts 
+        gPoly.addItem(new Item("milk", 10));
+        gPoly.addItem(new Item("eggs", 8));
+        gPoly.printReceipt();
+        System.out.println("Total Price = $" + g.getTotal() + " - with discount");
     }
     
 }
