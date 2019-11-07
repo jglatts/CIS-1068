85c773d30cf24d4e7ced316d12721bb7cabda821
John Glatts <john@z-axiscc.com>
2019-11-06 22:50:48 -0500

    Update FileReverse.java

--- Glatts_John_Assignment_7/src/glatts_john_assignment_7/FileReverse.java
+++ Glatts_John_Assignment_7/src/glatts_john_assignment_7/FileReverse.java
@@ -1,15 +1,39 @@
 package glatts_john_assignment_7;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FileReverse {
     
-    File file;
+    protected File file;
+    protected ArrayList<String> list = new ArrayList<>();  
     
-    public FileReverse(File f) {
+    public FileReverse(File f) throws FileNotFoundException {
         this.file = f;
+        readFile();
     }
     
-    // add method to reverse the file itself and one for sentances
+    private void readFile() throws FileNotFoundException {
+        Scanner scan = new Scanner(file);
+        while(scan.hasNext()) {
+            list.add(scan.nextLine());
+        }
+    }
+    
+    public void reverse() {
+        ArrayList<String> newList = new ArrayList<>();
+        int count = 0;
+        for(int i = list.size()-1; i > 0; --i, ++count)
+              newList.add(list.get(i));
+        this.list = newList;
+    }
+    
+    public void printFile() {
+        list.forEach((s) -> {
+            System.out.println(s);
+        });
+    }   
     
 }
