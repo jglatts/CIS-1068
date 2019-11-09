71df1fb42f2daa98f817d826f150a8402cf0f49b
John Glatts <john@z-axiscc.com>
2019-11-08 21:05:41 -0500

    Update FileReverse.java

--- Glatts_John_Assignment_7/src/glatts_john_assignment_7/FileReverse.java
+++ Glatts_John_Assignment_7/src/glatts_john_assignment_7/FileReverse.java
@@ -2,19 +2,33 @@ package glatts_john_assignment_7;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileReverse {
     
-    private File file;
-    private ArrayList<String> list = new ArrayList<>();  
+    private final File file;
+    private final ArrayList<String> list = new ArrayList<>();  
+    private final ArrayList<String> revList = new ArrayList<>();
+    private final ArrayList<String> swappedList = new ArrayList<>();
+    private final String revFileName = "reversed_file.txt";
+    private final String swapFileName = "swapped_file.txt";
+    private final PrintStream reversedFile = new PrintStream(new File(revFileName)); 
+    private final PrintStream swappedFile = new PrintStream(new File(swapFileName));
     
+    /*
+        Construct a new FileReverse with the given file
+    */ 
     public FileReverse(File f) throws FileNotFoundException {
         this.file = f;
         readFile();
     }
     
+    /*
+        Read the given file and add to a new list
+    */     
     private void readFile() throws FileNotFoundException {
         Scanner scan = new Scanner(file);
         while(scan.hasNext()) {
@@ -22,17 +36,67 @@ public class FileReverse {
         }
     }
     
-    public void reverse() {
-        ArrayList<String> newList = new ArrayList<>();
-        for(int i = list.size()-1; i >= 0; --i)
-              newList.add(list.get(i));
-        this.list = newList;
+    /*
+        Reverse the whole file
+    */       
+    public void reverse() throws IOException {
+        for (int i = list.size()-1; i >= 0; --i) {
+            String[] words = list.get(i).split(" ");
+            String newStr = "";
+            for (int x = words.length-1; x >= 0; --x) {
+                newStr += words[x] + " ";
+            }
+            revList.add(newStr);
+        }      
+        this.printReverseFile(reversedFile, revList);
     }
     
-    public void printFile() {
+    /*
+        Reverse the words on each line of the list
+    */       
+    public void reverseLines() throws IOException {
+        for (int i = 0; i < list.size(); ++i) {
+            String[] words = list.get(i).split(" ");
+            String newStr = "";
+            for (int x = words.length-1; x >= 0; --x) {
+                newStr += words[x] + " ";
+            }
+            swappedList.add(newStr);
+        }
+        this.printReverseFile(swappedFile, swappedList);
+    }
+    
+    /*
+        Print the unaltered list
+    */       
+    public void printFileString() {
         list.forEach((s) -> {
             System.out.println(s);
         });
-    }   
+    }     
+        
+    /*
+        Return the reversed file location name
+    */
+    public String getReversedFileName() {
+       return revFileName;     
+    }
+ 
+    /*
+        Return the reversed and swapped file location name
+    */    
+    public String getSwappedFileName() {
+       return swapFileName;    
+    }
+    
+    /*
+        Print the reversed file to the given location
+    */      
+    private void printReverseFile(PrintStream p, ArrayList<String> l) {
+        for (int i = 0; i < l.size(); ++i) {
+                p.print(l.get(i)); 
+                p.println();
+        }        
+    }
     
 }
