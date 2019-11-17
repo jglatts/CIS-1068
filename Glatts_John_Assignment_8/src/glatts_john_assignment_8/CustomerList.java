6ba6e6922ef38989769622da5fbae54f0e5ec11a
John Glatts <john@z-axiscc.com>
2019-11-17 08:26:44 -0500

    Update CustomerList.java

--- Glatts_John_Assignment_8/src/glatts_john_assignment_8/CustomerList.java
+++ Glatts_John_Assignment_8/src/glatts_john_assignment_8/CustomerList.java
@@ -6,51 +6,91 @@ import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * CustomerList Class
+ *      - Mimics ArrayLists with a few extra features 
+ * 
+ * 
+ * ToDo
+ *  - Check all methods for completion
+ *  - Make sure the file read/write are working
+ *      - seem to be getting a 'off-by-one' error on the write()
+ * 
+ * @author johng
+ */
+
 public class CustomerList {
     
     private int size;
     private int idx = 0;
     private Customer[] custArray;
     
+    /**
+     * Default Constructor
+     * Set the length of array to 4
+     */
     public CustomerList() {
         this.size = 4;
         custArray = new Customer[4];
     }
     
-    public CustomerList(int s) {
-        this.size = s;
-        custArray = new Customer[size];
+    /**
+     * CustomerList constructor with int parameter
+     * @param i, the size of the new array
+     */
+    public CustomerList(int i) {
+        this.size = i;
+        custArray = new Customer[i];
     }
     
+    /**
+     * @return the arrays size
+     */
     public int size() { 
         return size; 
     }
     
+    /**
+     * Get the customer at the given array, if it exists
+     * @param i, the index of the customer in the array
+     * @return the customer if it exists, null otherwise
+     */
     public Customer get(int i) {
         if (i < (size-1))
             return custArray[i];
         return null;
     }
     
+    /**
+     * Add a new customer at the given index
+     * @param c, the customer to add
+     * @param i, the index of to add the customer at
+     * @return true if completed, false otherwise
+     */
     public boolean set(Customer c, int i) {
-        if (i < (size-1)) {
+        if (i < size) {
             custArray[i] = c;
             return true;
         }            
         return false;
     }
     
+    /**
+     * Add a new customer to the array
+     * If the array is full, make a new array twice the size 
+     * and transfer the data
+     * 
+     *  - DOUBLE CHECK THIS GUY
+     * 
+     * @param c, the customer to add
+     */
     public void add(Customer c) {
         Customer[] tempArray;
-
         if(custArray[size-1] == null) {
-            if (idx < size) {
-                custArray[idx] = c;
-                idx++;
-            }    
+            custArray[idx] = c;
+            idx++;
         }
         else {
-            // prolly have to tweak this
             int lastIndex = findLastIndex();
             size *= 2;
             tempArray = new Customer[size];
@@ -61,6 +101,11 @@ public class CustomerList {
         }
     }
     
+    /**
+     * Find the the last index of array that has a value
+     * This method is used when making a new array
+     * @return the index of the last value in array
+     */
     private int findLastIndex() {
         int count = 0;
         for (int i = 0; i < size; ++i) {
@@ -70,12 +115,20 @@ public class CustomerList {
         return count;
     }
     
+    /**
+     * Remove the customer at the given index from the list
+     * @param i, the index to remove the customer
+     * @return the customer if completed, null otherwise
+     * 
+     *  ToDo
+     *      - change the size of the array after a remove() is called
+     *          - i.e, swawp every value to i+1 indices
+     * 
+     */
     public Customer remove(int i) {
         if (i < findLastIndex()) {
-            // have to do more here, tweak the array 
             Customer t = custArray[i];
             custArray[i] = null;
-            // call a method to tweak the Array here
             return t;
         }    
         return null;
@@ -92,6 +145,14 @@ public class CustomerList {
         return s + " Total Gross Sales = " + total;
     }
     
+    /**
+     * Read a file that contains a CSV string
+     * Instantiate a new Customer with the CSV string and add them to the list
+     * 
+     * @param fileName, the file to read
+     * @return the newList if completed, null otherwise
+     * @throws FileNotFoundException 
+     */
     public static CustomerList read(String fileName) throws FileNotFoundException {
         File custInput = new File(fileName);
         Scanner scan = new Scanner(custInput);
@@ -111,6 +172,13 @@ public class CustomerList {
         else return null;
     }
     
+    /**
+     * Write the current CustomerList to the given file
+     * @param cList, the CustomerList to write to file
+     * @param f, the file to write to
+     * @return true if completed, false otherwise
+     * @throws FileNotFoundException 
+     */
     public boolean write(CustomerList cList, String f) throws FileNotFoundException {
         PrintStream output = new PrintStream(new File(f));
         try {
@@ -124,10 +192,26 @@ public class CustomerList {
         return false;
     }
     
+    /**
+     * Sort the CustomerList by CustomerID
+     * 
+     * ----NOT WORKING----
+     * 
+     */
     public void sort() {
         Arrays.sort(custArray);
     }
     
+    /**
+     * Find the index of a Customer with the CustomerID as the argument
+     * @param i, the customerID to look for
+     * @return the index if it is found, the new added index if it was not found
+     * 
+     *  ----ToDo----
+     *      -  add the new value if it doesn't already exist
+     *      -  then update the array (change its size/indices etc...)
+     * 
+     */
     public int indexOf(int i) {
         this.sort();    
         int l = 0, r = custArray.length - 1; 
