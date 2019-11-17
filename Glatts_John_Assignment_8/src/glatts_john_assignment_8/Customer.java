f042503983db36f836a08f44bdd8a4dfc3cc7fd8
John Glatts <john@z-axiscc.com>
2019-11-17 08:27:16 -0500

    Update Customer.java

--- Glatts_John_Assignment_8/src/glatts_john_assignment_8/Customer.java
+++ Glatts_John_Assignment_8/src/glatts_john_assignment_8/Customer.java
@@ -1,19 +1,41 @@
 package glatts_john_assignment_8;
 
+/**
+ * Customer Class
+ *  - Inherits Person and uses the Comparable interface
+ * 
+ * ------ToDo------
+ *      - get the compareTo() working
+ *      - double check all methods
+ * 
+ * @author johng
+ */
 public class Customer extends Person implements Comparable{
     
     private int customerID;
     private double grossSales;
     
-    /***** Add the Default Constructors *****/
-    
-    
+    /**
+     * Constructor with all the data passed in as parameters
+     * @param f, firstName of customer
+     * @param l, lastName of customer
+     * @param a, address of customer
+     * @param c, city of customer
+     * @param s, state of customer
+     * @param z, zipCode of customer
+     * @param cID, customerID of customer
+     * @param g, grossSales of customer 
+     */
     public Customer(String f, String l, int a, String c, String s,  int z, int cID, double g) {
         super(f, l, a, c, s, z);
         this.customerID = cID;
         this.grossSales = g;
     }
     
+    /**
+     * Constructor with a CSV string as a parameter
+     * @param s, the CSV string containing all necessary data
+     */
     public Customer(String s) {
         super(s);  // debug this
         this.copyCSV(s);
@@ -45,12 +67,23 @@ public class Customer extends Person implements Comparable{
         return customerID + ", " + grossSales + ", " + super.toCSV();
     }
     
+    /**
+     * Compare the current Customer to the Customer passed as a parameter
+     * Check which one has the smaller customerID number
+     * 
+     * @param c, the Customer to compare to
+     * @return the value indicating which customer ID is smaller
+     */
     public int compareTo(Customer c) {
         if (this.customerID > c.getCustomerID()) return -1;
         if (this.customerID < c.getCustomerID()) return 1;
         return 0;
     }
     
+    /**
+     * Copy Customer data passed as a parameter to the this current Customer 
+     * @param c, the Customer to copy data from
+     */
     public void copy(Customer c) {
         this.customerID = c.getCustomerID();
         this.grossSales = c.getCustomerID();
@@ -61,6 +94,10 @@ public class Customer extends Person implements Comparable{
         super.setZipCode(c.getZipCode(true));
     }
     
+    /**
+     * Given a CSV string as a parameter, extract all the necessary data from it
+     * @param s, the CSV string
+     */
     public void copyCSV(String s) {
         String[] data = s.split(", ");
         this.customerID = Integer.parseInt(data[0]);
@@ -81,10 +118,7 @@ public class Customer extends Person implements Comparable{
 
     @Override
     public int compareTo(Object t) {
-        Customer c = (Customer)t;
-        if (this.customerID > c.getCustomerID()) return -1;
-        if (this.customerID < c.getCustomerID()) return 1;
-        return 0;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
