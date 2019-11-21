b1a69b640578022b33b889e69cc8dbc0c62b050d
John Glatts <john@z-axiscc.com>
2019-11-20 22:16:51 -0500

    Update Sorting.java

--- Random Spaghetti/Sorting.java
+++ Random Spaghetti/Sorting.java
@@ -1,34 +1,73 @@
-package sorting;
+/*
+ * The MIT License
+ *
+ * Copyright 2019 johng.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package trees;
 
 import java.util.Arrays;
 
 /**
- * Whaddup Witit
+ *
  * @author johng
  */
-public class Sorting {
+public class Trees {
 
     public static void main(String[] args) {
-        int[] arr = {3, 420, 69, 65, 23, 1, 54, 20, 19, 45};
+        Node root = new Node(5);
+        root.left = new Node(3);
+        root.right = new Node(8);
+        root.right.left = new Node(5);
+        root.right.right = new Node(13);
+        root.left.left = new Node(1);
+        root.left.right = new Node(4);
+        System.out.println("Searching a binary tree!! Is the value in the tree? " + ifNodeExists(root, 4));
+        System.out.println("Searching a binary tree!! Is the value in the tree? " + ifNodeExists(root, 24));
+        System.out.println("There are " + root.count + " nodes");
+        
+        int[] arr = {69, 56, 34, 21, 7, 31, 20, 1, 4};
         Arrays.sort(arr);
-        System.out.println("Sorted Array --> " + Arrays.toString(arr));
-        System.out.println("Found 69 at index: " + binarySearch(arr, 69));
+        System.out.println("" + Arrays.toString(arr));
+        System.out.println("The index of 31 is " + binarySearch(arr, 0, arr.length-1, 31));
     }
     
-    public static int binarySearch(int[] arr, int target) {
-        int min = 0;
-        int max = arr.length-1;
-        while (min <= max) {
-            int mid = (min+max)/2;
-            if (arr[mid] < target) {
-                min = mid + 1;
-            } 
-            else if (arr[mid] > target) {
-                max = mid-1;
+    public static boolean ifNodeExists(Node b, int val) {
+        if (b == null) return false;  
+        if (b.key == val) return true;  
+        boolean res1 = ifNodeExists(b.left, val);  
+        boolean res2 = ifNodeExists(b.right, val);  
+        return res1 || res2;  
+    }              
+    
+    public static int binarySearch(int[] arr, int min, int max, int value) {
+        if (max >= min) {
+            int mid = (min+(max-1)) / 2;
+            if (arr[mid] == value){ 
+                return mid;
+            }    
+            if (arr[mid] > value) {
+                return binarySearch(arr, min, mid-1, value);
             }
-            else return mid;
+            return binarySearch(arr, mid+1, max, value);
         }
-        return min;
+        return -1;
     }
-    
 }
